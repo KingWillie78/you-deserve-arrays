d41a168eb79daaacfc6633d21099c98c13977bef
“Willie” <willie.williams@codeimmersives.com>
2021-06-10 14:08:47 -0400

    finished

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/******************
- * YOUR CODE HERE *
- ******************/
-
 function getFirstItemFrom(array){
   return array[0];
 }
@@ -30,52 +26,4 @@ function secondCharOfThirdString(array) {
   if (typeof array[2] === 'string') {
     return (array[2][1]);
   }
-}
- /********************************************
- * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
- *           PLEASE DON'T TOUCH!            *
- ********************************************/
-
-
-
-if (typeof getFirstItemFrom === 'undefined') {
-  getFirstItemFrom = undefined;
-}
-
-
-if (typeof getLastItemFrom === 'undefined') {
-  getLastItemFrom = undefined;
-}
-
-
-
-if (typeof getIndex3 === 'undefined') {
-  getIndex3 = undefined;
-}
-
-
-if (typeof firstItemIsNumber === 'undefined') {
-  firstItemIsNumber = undefined;
-}
-
-
-if (typeof isLongList === 'undefined') {
-  isLongList = undefined;
-}
-
-
-if (typeof secondCharOfThirdString === 'undefined') {
-  secondCharOfThirdString = undefined;
-}
-
-
-
-
-module.exports = {
-  getLastItemFrom,
-  getFirstItemFrom,
-  getIndex3,
-  firstItemIsNumber,
-  isLongList,
-  secondCharOfThirdString,
 }
